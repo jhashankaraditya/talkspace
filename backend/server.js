84c671da746c888ee03153a7fe50cb852f7b2821
Aditya Shankar Jha <adityashankarjha_se21a13_37@dtu.ac.in>
2024-07-21 17:42:38 +0530

    modified server configurations

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -7,7 +8,9 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-dotenv.config({ path: '../.env' });
+dotenv.config({ path1: '../.env' });
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000
 
@@ -17,6 +20,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT,() => {
     connectToMongoDB();
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -27,7 +27,7 @@ const SignUp = () => {
         <div className="flex flex-col items-centerjustify-center min-w-96 mx-auto ">
             <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 ">
                 <h1 className="text-3xl font-semibold text-center text-white">
-                    SignUp <span className="text-blue-500">ChatApp</span>
+                    SignUp <span className="text-blue-500">TalkSpace</span>
                 </h1>
                 <form onSubmit={handleSubmit}>
                     <div>
